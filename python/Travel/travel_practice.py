142b297517d2f1ca0d0572b8087e55c904eb0790
수박이 <102145617+subak222@users.noreply.github.com>
2022-09-28 10:37:28 +0900

    20220928_python_1~2

---
+++ python/Travel/travel_practice.py
@@ -0,0 +1,13 @@
+import imp
+#import travel.Thailand
+
+#trip_to = travel.Thailand.ThailandPackage()
+#trip_to.detail()
+
+
+from travel import Vietnam
+
+trip_to = Vietnam.VietnamPackage()
+trip_to.detail()
+
+
---
+++ python/practice.py
@@ -0,0 +1,42 @@
+#import theater_module
+#theater_module.price(3)
+#theater_module.price_morning(4)
+#theater_module.price_soldier(5)
+
+#패키지를 불러올 때 많이 사용함
+#from theater_module import * 
+#price(3)
+#price_morning(4)
+#price_soldier(5)
+
+#import theater_module as mv
+#mv.price(3)
+#mv.price_morning(4)
+#mv.price_soldier(5)
+
+#from theater_module import price, price_morning, price_soldier
+#price(3)
+#price_morning(4)
+#price_soldier(5)
+
+#from theater_module import price_soldier as price
+#price(5)
+
+#-------------------------------------------------------------
+
+#import datetime as dt
+
+#print('오늘 =', dt.datetime.now())
+
+#hundraed = dt.timedelta(days = 100)
+#plus100day = dt.datetime.now() + hundraed
+#print('100일 후 =', plus100day)
+
+#minus100day = dt.datetime.now() - hundraed
+#print('100일 전 =', minus100day)
+
+#oneweek = dt.timedelta(weeks = 1)
+#nextweek = dt.datetime.now() - oneweek
+#print('1주일 후 =', nextweek)
+
+#-------------------------------------------------------------
